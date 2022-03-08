f59c26e7aaf1dc2c6367f7b8b1dcc8d9fb133558
svensk007 <83252177+svensk007@users.noreply.github.com>
2022-03-08 12:34:51 -0600

    Functionality
    added more functions and fixed typos

--- Main.gd
+++ Main.gd
@@ -6,3 +6,24 @@ func _ready():
 
 func _on_Exit_pressed():
 	get_tree().quit()
+
+
+func _on_OpenFile_pressed():
+	$OpenFile/OpenFileDialog.popup_centered()
+
+
+func _on_SaveFile_pressed():
+	$SaveFile/SaveFileDialog.popup_centered()
+
+
+func _on_OpenFileDialog_file_selected(path):
+	print(path)
+	var f = File.new()
+	f.open(path, 1)
+	$TextEdit.text = f.get_as_text()
+
+
+func _on_SaveFileDialog_file_selected(path):
+	var f = File.new()
+	f.open(path, 2)
+	f.store_string($TextEdit.text)
